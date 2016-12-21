e9637576196a5cbe25ddde2f7493f22692c32d24
Travis Wood <travis_cw@yahoo.com>
2016-12-21 14:51:15 -0800

    Create basic link item positioning / formatting

--- src/Link.js
+++ src/Link.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './styles/Link.css';
 
-const Link = ({data}) => (
-  <div>
-    <img src={data.thumbnail} />
-    <a href={data.url}> {data.title} </a>
-    <p> {data.author} </p>
-  </div>
-)
+const Link = ({data}) => {
+  console.log('thumbnail data', data.thumbnail);
+  
+  // Use a default question mark image if no image is provided
+  const image = data.thumbnail === 'self' || data.thumbnail === 'default' ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Blue_Question.svg/128px-Blue_Question.svg.png' : data.thumbnail;
+
+  return (
+    <div className="linkContainer">
+      <div className="imageContainer">
+        <img className="thumbnail" src={image} />
+      </div>
+      <div className="info">
+        <a className="title" href={data.url}> {data.title} </a>
+        <p className="author"> {data.author} </p>
+      </div>
+    </div>
+  )
+}
 
 export default Link;
