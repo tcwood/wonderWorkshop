4dff74dbb1bc322be778ec91ff6de3141bcb1084
Travis Wood <travis_cw@yahoo.com>
2016-12-21 13:43:14 -0800

    Create GET req for front page items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends React.Component {
+  componentWillMount() {
+    axios.get('http://www.reddit.com/.json?raw_json=1')
+    .then( (res) => console.log('axios res', res));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Hello Wonder Workshop!</h2>
         </div>
-
       </div>
     );
   }
