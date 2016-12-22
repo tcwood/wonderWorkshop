1d567bfc0ba2006b4244a0681156719440b52542
Travis Wood <travis_cw@yahoo.com>
2016-12-21 19:23:13 -0800

    format links and add counter

--- src/Link.js
+++ src/Link.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './styles/Link.css';
 
-const Link = ({data}) => {
+const Link = ({data, index}) => {
   // Use a default question mark image if no image is provided
   const image = data.thumbnail === 'self' || data.thumbnail === 'default' ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Blue_Question.svg/128px-Blue_Question.svg.png' : data.thumbnail;
-
   return (
-    <div className="linkContainer">
-      <div className="imageContainer">
+    <div className="link-container">
+      <h1 className="count"> {index + 1} </h1>
+      <div className="image-container">
         <img className="thumbnail" src={image} alt={data.title} />
       </div>
       <div className="info">
         <a className="title" href={data.url}> {data.title} </a>
-        <p className="author"> {data.author} </p>
+        <div className="bottom-right">
+          <p className="small-content"> {`Author: ${data.author}`}</p>
+          <p className="small-content"> {`Subreddit: ${data.subreddit}`} </p>
+        </div>
       </div>
     </div>
   )
--- src/LinkList.js
+++ src/LinkList.js
@@ -3,9 +3,9 @@ import Link from './Link';
 import './styles/LinkList.css';
 
 const LinkList = ({info}) => (
-  <div className="LinkList">
+  <div className="link-list">
     {info.map((item, index) => 
-      (<Link key={index} data={item.data}/>)
+      (<Link key={index} index={index} data={item.data}/>)
     )}
   </div>
 )
